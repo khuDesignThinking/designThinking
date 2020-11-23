18328dbc7f69407d3f0076adf4f8906ce923b490
hyeneung <dkwjd0824@khu.ac.kr>
2020-11-24 01:31:05 +0900

    [add] rename file

---
+++ FileRename/GUI_graph.py
@@ -0,0 +1,79 @@
+import cv2
+import sys
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
+import draw_graph as dg
+
+
+class ShowVideo(QtCore.QObject):
+    image_directory = "dataset/graph.jpg"
+    dg.update_graph(image_directory)
+
+    image = cv2.imread(image_directory, cv2.IMREAD_COLOR)
+
+    height, width = image.shape[:2] # 사진의 픽셀을 확인함.
+
+    VideoSignal1 = QtCore.pyqtSignal(QtGui.QImage)
+
+    run_program = True
+
+    def __init__(self, parent=None):
+        super(ShowVideo, self).__init__(parent)
+
+    def stopVideo(self):
+        self.run_program = False
+        return None
+
+    @QtCore.pyqtSlot()
+    def startVideo(self):
+        self.run_program = True
+        while self.run_program:
+            dg.update_graph()
+            image = cv2.imread(self.image_directory, cv2.IMREAD_COLOR)
+            color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            qt_image1 = QtGui.QImage(color_swapped_image.data, # ndarray data attribute 버퍼객체의 시작을 가르킨다는데 잘 모르겠음.
+                                    self.width,
+                                    self.height,
+                                    color_swapped_image.strides[0],
+                                    QtGui.QImage.Format_RGB888)
+            self.VideoSignal1.emit(qt_image1)
+            loop = QtCore.QEventLoop()
+            QtCore.QTimer.singleShot(1000, loop.quit) # every 10,000ms 10s update image
+            loop.exec_()
+    
+
+class ImageViewer(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(ImageViewer, self).__init__(parent)
+        self.image = QtGui.QImage()
+        self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
+        self.initUI()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        self.image = QtGui.QImage()
+
+    def initUI(self):
+        pass
+        # self.setWindowTitle('DESIGN THINKING PROJ')
+        # self.setWindowIcon(QIcon('./resource/images/networking.png'))
+        # self.move(300,300)
+        # self.resize(300,600)
+    
+    @QtCore.pyqtSlot(QtGui.QImage)
+    def setImage(self, image):
+        if image.isNull():
+            print("Viewer Dropped frame!")
+
+        self.image = image
+        if image.size() != self.size():
+            self.setFixedSize(image.size())
+        self.update()
+
+
+def check_sourcefile():
+    pass
---
+++ FileRename/draw_graph.py
@@ -0,0 +1,53 @@
+from matplotlib import pyplot as plt
+import GUI_main as gui
+
+
+def load_source(source_directory:str = "dataset/count_blink.csv"):
+    """
+    If there is no csv file, This function make count_blink.csv file
+    """
+    try: #Check whether count_blink.csv exist.
+        with open(source_directory, 'r') as f:
+            pass
+    except:
+        with open(source_directory, 'w') as f:
+            f.write("15, 12, 15, 16, 10, 14") # this is test data
+
+    with open(source_directory, 'r') as f:
+        data = f.read()
+    string_blink_count = data.split(",")
+    int_blink_count = [int(blink_data) for blink_data in string_blink_count]
+    return int_blink_count
+    
+
+def update_graph(image_directory: str = "dataset/graph.jpg"):
+    """
+    This function make "resource/images/graph.jpg" file
+    param: 
+        y_list : count blink.csv file's value
+        
+    var:
+        x_list : same lenth with y_list, indicate 10 second
+    raise: make count_blink.csv to graph.jpg, update graph.jpg
+    """
+    y_list = load_source()
+    y_lenth = len(y_list)
+    x_list = [f"{10 * sec} sec" for sec in range(y_lenth)]
+    plt.plot(x_list, y_list)
+    plt.xlabel('ten second')
+    plt.ylabel('Blink number')
+    plt.title('Blink count')
+    plt.savefig(image_directory)
+    plt.close()
+    if(len(x_list)>8):
+        del x_list[0]
+        del y_list[0]
+    return None
+    
+
+def main():
+    update_graph()
+
+
+if __name__ == "__main__":
+    main()
