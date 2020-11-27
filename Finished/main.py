2ec3ab1315cbaf3d4389db3720f7c759d50e2a8d
hyeneung <dkwjd0824@khu.ac.kr>
2020-11-27 16:29:50 +0900

    [add] finished

---
+++ Finished/main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from threading import Thread
+import time
+from GUI_main import Ui_MainWindow 
+
+
+class kinwriter(QMainWindow, Ui_MainWindow): 
+    
+    def __init__(self):
+
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.show()
+
+app = QApplication([])
+app.setQuitOnLastWindowClosed(False)
+sn = kinwriter()
+QApplication.processEvents()
+sys.exit(app.exec_())
